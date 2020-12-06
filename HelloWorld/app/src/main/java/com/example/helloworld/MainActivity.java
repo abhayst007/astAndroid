e72a9748c9b46b796d143fceda42e7d0330bcd6e
abhayst007 <abhayst2007@gmail.com>
2020-12-06 20:19:03 +0530

    initial version

---
+++ HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText num1, num2;
+    private Button sum;
+    private TextView answer;
+    private Button multiply;
+    private Button divide;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        addListenerOnButton();
+    }
+
+    public void addListenerOnButton() {
+
+        num1 = (EditText) findViewById(R.id.num1);
+        num2 = (EditText) findViewById(R.id.num2);
+        sum = (Button) findViewById(R.id.calcSum);
+        answer = (TextView) findViewById(R.id.sum);
+        multiply = (Button) findViewById(R.id.multiply);
+        divide = (Button) findViewById(R.id.divide);
+
+        sum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String value1 = num1.getText().toString();
+                String value2 = num2.getText().toString();
+                int a = Integer.parseInt(value1);
+                int b = Integer.parseInt(value2);
+                int sum = a + b;
+                answer.setText(Integer.toString(sum));
+                Toast.makeText(getApplicationContext(), String.valueOf(sum), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        multiply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String value1 = num1.getText().toString();
+                String value2 = num2.getText().toString();
+                int a = Integer.parseInt(value1);
+                int b = Integer.parseInt(value2);
+                int sum = a * b;
+                answer.setText(Integer.toString(sum));
+            }
+        });
+
+
+        divide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String value1 = num1.getText().toString();
+                String value2 = num2.getText().toString();
+                int a = Integer.parseInt(value1);
+                int b = Integer.parseInt(value2);
+                int sum = a / b;
+                answer.setText(Integer.toString(sum));
+            }
+        });
+    }
+
+}
